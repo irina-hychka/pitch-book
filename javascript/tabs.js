c08c91498a8e6ffd1133b6d2713a40806ee296d3
Irina <irishchest@gmail.com>
2019-06-08 11:31:35 +0300

    Done tabs clickable

---
+++ javascript/tabs.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+
+    $('ul.tabs li').click(function () {
+        var tab_id = $(this).attr('data-tab');
+
+        $('ul.tabs li').removeClass('active');
+        $('.tab-contnent--wrap').removeClass('active');
+
+        $(this).addClass('active');
+        $("#" + tab_id).addClass('active');
+    })
+
+})
